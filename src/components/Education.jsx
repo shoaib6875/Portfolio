4f55c51464b1fc1bf72f9e786100c5f842e434ee
Shoaib Akhtar <shoaibakhtar14082002@gmail.com>
2025-08-31 21:35:15 +0530

    fifth commit for Portfolio

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -14,15 +14,15 @@ const Education = ({ isDarkMode }) => {
               <div className="text-4xl mb-4">🎓</div>
               <h3 className={`text-2xl font-bold mb-2 ${isDarkMode ? 'text-gray-800' : 'text-gray-800'}`}>B.Tech in Information Technology</h3>
               <p className={`text-lg text-blue-600 font-semibold mb-2 ${isDarkMode ? 'text-blue-400' : 'text-blue-600'}`}>Maharaja Surajmal Institute of Technology, Delhi</p>
-              <p className={`text-gray-500 mb-3 ${isDarkMode ? 'text-gray-400' : 'text-gray-500'}`}>2024 - 2027</p>
+              <p className={`text-gray-500 mb-3 ${isDarkMode ? 'text-gray-400' : 'text-gray-500'}`}>2024 - 2027 (ongoing)</p>
               <p className={`text-gray-600 mb-4 ${isDarkMode ? 'text-gray-300' : 'text-gray-600'}`}>
-                Pursuing a B.Tech with a focus on full-stack development, DevOps, and AI, with hands-on projects in AWS and MERN stack.
+                Pursuing a B.Tech with a focus on core subjects like programming, daya structure, mathematics, dbms ,web development and AI.
               </p>
               <div className="flex flex-wrap gap-2">
                 <span className="bg-blue-100 text-blue-800 px-3 py-1 rounded-full text-sm font-medium">Object oriented programming</span>
                 <span className="bg-purple-100 text-purple-800 px-3 py-1 rounded-full text-sm font-medium">Applied Mathematics</span>
                 <span className="bg-purple-100 text-yellow-800 px-3 py-1 rounded-full text-sm font-medium">Data stucture</span>
-                <span className="bg-green-100 text-green-800 px-3 py-1 rounded-full text-sm font-medium">AI/ML</span>
+                <span className="bg-green-100 text-green-800 px-3 py-1 rounded-full text-sm font-medium">Java programming</span>
               </div>
             </div>
           </div>
